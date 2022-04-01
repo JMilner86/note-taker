4182661372e06ceaaa7094b97d5bd632f0a494ee
Jonah Milner <jonahmilner86@gmail.com>
2022-04-01 13:43:49 -0600

    Worked on get and post requests

--- server.js
+++ server.js
@@ -4,3 +4,16 @@ const express = require('express');
 
 
 const app = express();
+
+app.get("/api/notes", (req, res) => {
+    res.json()
+});
+
+app.post("/api/notes", (req, res) => {
+
+});
+
+
+app.listen(3001, () => {
+    console.log(`API server now on port 3001!`);
+  });
