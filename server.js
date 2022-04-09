ba03651f3c11fb150a948cef839c436cf3c11bb7
Jonah Milner <jonahmilner86@gmail.com>
2022-04-08 18:31:29 -0600

    added router and server reqs

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
 const express = require('express');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+const fs = require('fs');
+const path = require('path')
 
-
-
-
+const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.get("/api/notes", (req, res) => {
-    res.json()
-});
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
-app.post("/api/notes", (req, res) => {
 
-});
-
-
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
   });
